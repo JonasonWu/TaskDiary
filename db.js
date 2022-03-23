29de2b66fdf9a87706e6549ca5f0c91457e50e4c
JonasonWu <jw5911@nyu.edu>
2022-03-23 12:36:50 -0400

    The schemas are somewhat done. May need more info from future lectures to make it better.

--- db.js
+++ db.js
@@ -1,34 +1,66 @@
 const mongoose = require('mongoose'),
 	URLSlugs = require('mongoose-url-slugs'),
-  passportLocalMongoose = require('passport-local-mongoose');
+  	passportLocalMongoose = require('passport-local-mongoose');
 
 
 const User = new mongoose.Schema({
-  // username, password
-  lists:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'List' }]
+	username: {type: String, required: true, minlength: 3},
+    password: {type: String, required: true}, //We will store the hash+salt together
+	CurrentTasks: [],
+	CompletedTasks: [],
+	Diary: []
+  	// lists:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'List' }]
 });
 
-const Item = new mongoose.Schema({
-	name: {type: String, required: true},
-	quantity: {type: Number, min: 1, required: true},
-	checked: {type: Boolean, default: false, required: true}
-}, {
-	_id: true
-});
-
-
-const List = new mongoose.Schema({
-  user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
-  name: {type: String, required: true},
-	createdAt: {type: Date, required: true},
-	items: [Item]
+const CurrentTasks = new mongoose.Schema({
+	user: String, // a reference to a User object
+	createdAt: String, //Time of creation of this object
+	title: String, //The subject or focus of the task
+	taskDetails: [], //an array that stores details of the task (via bullet points).
+	estimatedCompletionTime: Number //The amount of time that it may take to finish the task 
 });
 
 
-User.plugin(passportLocalMongoose);
-List.plugin(URLSlugs('name'));
+const CompletedTasks = new mongoose.Schema({
+	user: String, // a reference to a User object
+	createdAt: String, //Time of creation of this object
+	title: String, //The subject or focus of the task
+	taskDetails: [], //an array that stores details of the task (via bullet points).
+	estimatedCompletionTime: Number, //The amount of time that it may take to finish the task 
+	completedAt: String
+});
+
+const Diary = new mongoose.Schema({
+	user: String, // a reference to a User object.
+	createdAt: String, //Time of creation of this object. 
+	date: String, //The date the diary is for.
+	title: String, //The subject or focus of the diary. (data + title will be the title of the diary page shown to the user)
+	details: String, //The diary record for the day
+	completedAt: String //Finish time of the task
+});
+
+// const Item = new mongoose.Schema({
+// 	name: {type: String, required: true},
+// 	quantity: {type: Number, min: 1, required: true},
+// 	checked: {type: Boolean, default: false, required: true}
+// }, {
+// 	_id: true
+// });
+
+
+// const List = new mongoose.Schema({
+//   user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
+//   name: {type: String, required: true},
+// 	createdAt: {type: Date, required: true},
+// 	items: [Item]
+// });
+
+
+// User.plugin(passportLocalMongoose);
+// List.plugin(URLSlugs('name'));
 
 mongoose.model('User', User);
-mongoose.model('List', List);
-mongoose.model('Item', Item);
-mongoose.connect('mongodb://localhost/grocerydb');
+mongoose.model('CurrentTasks', CurrentTasks);
+mongoose.model('CompletedTasks', CompletedTasks);
+mongoose.model('Diary', Diary);
+mongoose.connect('mongodb://localhost/TaskDiary');
