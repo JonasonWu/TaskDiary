212c6dca9b873cc2e0c9e29dd55ae2681a4c976b
JonasonWu <jw5911@nyu.edu>
2022-04-24 14:08:16 -0400

    Fixed bug

--- app.js
+++ app.js
@@ -17,10 +17,26 @@ app.set('view engine', 'hbs');
 //Enable sessions
 const session = require('express-session');
 const sessionOptions = {
-    secret: process.env.SECRET,
     resave: true,
     saveUninitialized: true
 };
+//Set the secret value for sessionOptions
+if (process.env.NODE_ENV === 'PRODUCTION') {
+	// if we're in PRODUCTION mode, then read the configration from a file
+	// use blocking file io to do this...
+	const fs = require('fs');
+	const path = require('path');
+	const fn = path.join(__dirname, 'config.json');
+	const data = fs.readFileSync(fn);
+
+	// our configuration file will be in json, so parse it and set the
+	// conenction string appropriately!
+	const conf = JSON.parse(data);
+	sessionOptions.secret = conf.secret;
+} else {
+	// if we're not in PRODUCTION mode, then use random string
+	sessionOptions.secret = "secret string";
+}
 app.use(session(sessionOptions));
 
 app.use(passport.initialize());
